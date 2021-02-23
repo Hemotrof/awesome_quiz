214c1b786121d80c4890d649ae8646bcdd1945dc
MukhamedyanovIR <i.mukhamedyanov@modulbank.ru>
2021-02-23 18:00:50 +0500

    Initial Commit

---
+++ awesome_quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  awesome_quiz
+//
+//  Created by Идель Мухамедьянов on 23.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
